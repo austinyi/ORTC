eabde7777f78afea9b7d7b54c380da5cd7c402bc
phuongh0508 <135559766+phuongh0508@users.noreply.github.com>
2024-07-08 16:11:13 +0700

    Update entropicORTC.py
    minor changes

--- entropicORTC.py
+++ entropicORTC.py
@@ -14,23 +14,14 @@ from ortc_v1 import ortc_v1
 
 
 def entropic_ortc_new(A1, A2, c, eps, niter, delta):
-    # A1 -> w1
-    # A2 -> w2
-    # u, u' -> x
-    # v, v' -> y
-
     dx, _ = A1.shape
     dy, _ = A2.shape
 
-    # d1: dx
-    # d2: dy
-    # Also convert d1, d2 to different type
     d1 = np.sum(A1, axis=1)
     d2 = np.sum(A2, axis=1)
 
     # Init
     # C: dx * dy * dx * dy
-    # Convert C
     C = np.exp(-(c[:, :, np.newaxis, np.newaxis] + c[np.newaxis, np.newaxis, :, :]) / eps)
 
     # F: dx * dy * dx
@@ -51,7 +42,6 @@ def entropic_ortc_new(A1, A2, c, eps, niter, delta):
 
     # A mask for fast computation in the update of H
     # This mask have true everywhere, except for the ijij components
-    # Probably there is a more efficient way to creat this
     mask = np.ones((dx, dy, dx, dy), dtype=bool)
     for i in range(dx):
         for j in range(dy):
@@ -69,25 +59,20 @@ def entropic_ortc_new(A1, A2, c, eps, niter, delta):
     for _ in range(niter):
         if np.max(np.abs(w - w_old)) > delta:
             num_iter += 1
-            #print(np.max(np.abs(w - w_old)))
             w_old = np.copy(w)
 
             # 2: update F
             # d: dx * dy
             d = np.sum(w, axis=(2, 3))
-            # Sum over v', which is the third axis
             # t: sum((dx * dy * dx * dy ) * (dx * 1 * dx * dy) * (dx * dy * dx * dy) = dx * dy * dx * dy, axis=3)
             # t: dx * dy * dx
             t = np.sum(C * G[:, :, np.newaxis, :] * H * K, axis=3)
             # F: dx * dy * dx
             # For some reason (probably due to the limit in the float representation)
             # This way of calculating F can produce small errors ( <-1.38777878e-17) comparing to the loop
-            # Will need to investigate
             F = (d[:, :, np.newaxis] * A1[:, np.newaxis, :] / d1[:, np.newaxis, np.newaxis]) / t
 
             # 3: update G
-            d = np.sum(w, axis=(2, 3))
-            # Sum over u'
             # t: sum((dx * dy * dx * dy) * (dx * dy * dx * 1) * (dx * dy * dx * dy) = dx * dy * dx * dy, axis=2)
             # t: dx * dy * dy
             t = np.sum(C * F[:, :, :, np.newaxis] * H * K, axis=2)
