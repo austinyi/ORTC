c35d4f9d2ec8467891600f67e62158a7e2593436
phuongh0508 <135559766+phuongh0508@users.noreply.github.com>
2024-07-13 18:09:34 +0700

    Update entropicORTC.py
    Correct some minor mistakes

--- entropicORTC.py
+++ entropicORTC.py
@@ -12,11 +12,12 @@ import copy
 
 from ortc_v1 import ortc_v1
 
-
 def entropic_ortc_new(A1, A2, c, eps, niter, delta):
     dx, _ = A1.shape
     dy, _ = A2.shape
 
+    # d1: dx
+    # d2: dy
     d1 = np.sum(A1, axis=1)
     d2 = np.sum(A2, axis=1)
 
@@ -37,15 +38,8 @@ def entropic_ortc_new(A1, A2, c, eps, niter, delta):
     K = np.sum(F[:, :, :, np.newaxis] * C * G[:, :, np.newaxis, :] * H)
     K = 1 / K
 
-    # w: dx * dx * dx * dy
+    # w: dx * dx * dx * dy    
     w = C * F[:, :, :, np.newaxis] * G[:, :, np.newaxis, :] * H * K
-
-    # A mask for fast computation in the update of H
-    # This mask have true everywhere, except for the ijij components
-    mask = np.ones((dx, dy, dx, dy), dtype=bool)
-    for i in range(dx):
-        for j in range(dy):
-            mask[i, j, i, j] = False
             
     w_old = np.ones((dx,dy,dx,dy))
     num_iter = 0
@@ -81,16 +75,13 @@ def entropic_ortc_new(A1, A2, c, eps, niter, delta):
             G = (d[:, :, np.newaxis] * A2[np.newaxis, :, :] / d2[np.newaxis, :, np.newaxis]) / t
 
             # 4: update H
-            # Initialize H
-            H = np.ones((dx, dy, dx, dy))
-            # Create mask for the condition
             # Create the condition
             F_nonzero = F > 0
             G_nonzero = G > 0
             cond = np.logical_and(F_nonzero[:, :, :, np.newaxis], G_nonzero[:, :, np.newaxis, :])
 
             # The indices where we should update H
-            i, j, k, l = np.where(mask & cond)
+            i, j, k, l = np.where(cond)
             # Update H base on the mask and the condition
             H[i, j, k, l] = np.sqrt((F[k, l, i] * G[k, l, j]) / (F[i, j, k] * G[i, j, l]))
 
@@ -111,26 +102,30 @@ def entropic_ortc_new(A1, A2, c, eps, niter, delta):
     
     return w, exp_cost, num_iter, iter_history, cost_history
 
+# generate examples
+m1 = 3
+m2 = 2
+A1 = stochastic_block_model(np.array([m1,m1,m1,m1]), np.array([[1,0.1,0.1,0.1],[0.1,0.9,0.1,0.1],[0.1,0.1,0.8,0.1],[0.1,0.1,0.1,0.7]]))
+A2 = stochastic_block_model(np.array([m2,m2,m2,m2]), np.array([[1,0.1,0.1,0.1],[0.1,0.9,0.1,0.1],[0.1,0.1,0.8,0.1],[0.1,0.1,0.1,0.7]]))
+A1 = A1 / np.sum(A1)
+A2 = A2 / np.sum(A2)
+c = get_degree_cost(A1, A2)
+d1 = np.sum(A1, axis=1)
+d2 = np.sum(A2, axis=1)
 
+print(d1)
+print(d2)
 
-if __name__ == "__main__":
-    # generate examples to test
-    m1 = 3
-    m2 = 2
-    A1 = stochastic_block_model(np.array([m1,m1,m1,m1]), np.array([[1,0.1,0.1,0.1],[0.1,0.9,0.1,0.1],[0.1,0.1,0.8,0.1],[0.1,0.1,0.1,0.7]]))
-    A2 = stochastic_block_model(np.array([m2,m2,m2,m2]), np.array([[1,0.1,0.1,0.1],[0.1,0.9,0.1,0.1],[0.1,0.1,0.8,0.1],[0.1,0.1,0.1,0.7]]))
-    A1 = A1 / np.sum(A1)
-    A2 = A2 / np.sum(A2)
-    c = get_degree_cost(A1, A2)
-    d1 = np.sum(A1, axis=1)
-    d2 = np.sum(A2, axis=1)
-    print(d1)
-    print(d2)
+# test with entropicORTC
+start = time.time()
+w, exp_cost, n, iter_his, cost_his = entropic_ortc_new(A1, A2, c, 0.0001, 10000, 1e-10)
+print(exp_cost)
+end = time.time()
+print(end - start)
 
-
-    # test
-    start = time.time()
-    w, exp_cost, n, iter_his, cost_his = entropic_ortc_new(A1, A2, c, 0.0002, 10000, 1e-8)
-    print(exp_cost)
-    end = time.time()
-    print(end - start)
+# check with ortc_v1
+start = time.time()
+AA, BB = ortc_v1(A1, A2, c)
+print("ortc_v1 cost: ", AA)
+end = time.time()
+print("time for ortc_v1:", end - start)
